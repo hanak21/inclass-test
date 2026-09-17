cd6fd38f87fa90c8f6256e77bb6d63995cddaf70
hanak21 <hana.tk64@gmail.com>
2026-09-16 19:10:11 -0700

    added new shapes

--- sketch.js
+++ sketch.js
@@ -8,5 +8,9 @@ function draw() {
   background(20);
   fill(255, 120, 60);
   circle(mouseX, mouseY, 100);
-  
+  fill(222, 220, 60);
+  circle(200, 233, 100);
+  rect(320, 100, 100, 120);
+  fill(422, 30, 90);
+  rect(620, 300, 400, 120);
 }
