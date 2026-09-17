aec00c730c5375e6e69aa7cdac7fd8f0c1d6a5d4
hanak21 <hana.tk64@gmail.com>
2026-09-16 18:42:04 -0700

    Added background and moving mouse circle.

---
+++ sketch.js
@@ -0,0 +1,12 @@
+console.log("I believe in me");
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(20);
+  fill(255, 120, 60);
+  circle(mouseX, mouseY, 100);
+  
+}
